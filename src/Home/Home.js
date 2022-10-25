839ede4f727057e81e073fd6278ad0ef52cd1478
TutkuEroglu <rrandqua@gmail.com>
2022-10-25 05:51:10 +0300

    added searchLocation

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,34 +5,54 @@ import Background from "../Assets/bg.jpg";
 
 const Home = () => {
   const [location, setLocation] = useState("");
-
-  const options = {
-    method: 'GET',
-    url: 'https://weatherapi-com.p.rapidapi.com/history.json',
-    params: {q: 'Adana', dt: '2022-10-23', lang: 'en'},
-    headers: {
-      'X-RapidAPI-Key': '13a3d1c5a5mshaeff5a105d97999p16a63bjsnd83ec29df07e',
-      'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-    }
+  const [forecast, setForecast] = useState([]);
+  const [info, setInfo] = useState([]);
+  const initialState = {
+    locationName: "Adana",
+    locationTemp: "32.5 °C",
+    locationIcon: "//cdn.weatherapi.com/weather/64x64/day/113.png",
+    locationText: "Sunny",
+    locationFeels: "36 °C",
+    locationHumidity: "15",
+    locationWind: "8.4 MPH",
   };
-  
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-  }).catch(function (error) {
-      console.error(error);
-  });
 
-  const searchLocation = (e) => {
-    if (e.key === "Enter") {
-      console.log(location);
-      e.preventDefault();
+  const searchLocation = async (e) => {
+    const today = new Date();
+    try {
+      if (e.key === "Enter") {
+        e.preventDefault();
+        const options = {
+          method: "GET",
+          url: "https://weatherapi-com.p.rapidapi.com/history.json",
+          params: { q: `${location}`, dt: today, lang: "en" },
+          headers: {
+            "X-RapidAPI-Key":
+              "13a3d1c5a5mshaeff5a105d97999p16a63bjsnd83ec29df07e",
+            "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+          },
+        };
+
+        await axios
+          .request(options)
+          .then(function (response) {
+            setForecast(response?.data?.forecast?.forecastday[0]);
+            setInfo(response?.data?.location);
+            setLocation("")
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      }
+    } catch (err) {
+      console.log(err);
     }
   };
 
   return (
     <div className="Home">
-      <img src={Background} className="bgImage" alt="bg" />
-      <div className="Header">
+      <img src={Background} className="bgImage" alt="bg"/>
+      <form className="Header">
         <input
           className="searchInput"
           placeholder="Enter Location"
@@ -40,32 +60,61 @@ const Home = () => {
           onKeyDown={searchLocation}
           onChange={(e) => setLocation(e.target.value)}
           autoComplete=""
+          value={location}
         />
         <label className="bi bi-search searchIcon"></label>
-      </div>
+      </form>
       <div className="Container">
         <div className="Location">
-          <span>Adana</span>
+          <span>{info?.name ? info?.name : initialState.locationName}</span>
         </div>
         <div className="Temp">
-          <span>31°C</span>
-          <i className="bi bi-brightness-high-fill sunny"></i>
+          <img
+            src={
+              forecast?.day?.condition?.icon
+                ? forecast?.day?.condition?.icon
+                : initialState.locationIcon
+            }
+            className="condition"
+            alt="condition"
+          />
+          <span>
+            {forecast?.day?.mintemp_c
+              ? forecast?.day?.mintemp_c + " °C"
+              : initialState.locationTemp}{" "}
+          </span>
         </div>
         <div className="Description">
-          <span>Güneşli</span>
+          <span>
+            {forecast?.day?.condition?.text
+              ? forecast?.day?.condition?.text
+              : initialState.locationText}
+          </span>
         </div>
         <div className="Footer">
           <div className="FooterContain">
             <div className="Feels">
-              <p className="FeelsHeader">35°C</p>
+              <p className="FeelsHeader">
+                {forecast?.day?.avgtemp_c
+                  ? forecast?.day?.avgtemp_c
+                  : initialState.locationFeels}
+              </p>
               <p className="FeelsBottom">Feels Like</p>
             </div>
             <div className="Humidity">
-              <p className="HumidityHeader">70%</p>
+              <p className="HumidityHeader">
+                {forecast?.day?.avghumidity
+                  ? forecast?.day?.avghumidity
+                  : initialState.locationHumidity}
+              </p>
               <p className="HumidityBottom">Humidity</p>
             </div>
             <div className="Wind">
-              <p className="WindHeader">1.97 MPH</p>
+              <p className="WindHeader">
+                {forecast?.day?.maxwind_mph
+                  ? forecast?.day?.maxwind_mph + " MPH"
+                  : initialState.locationWind}
+              </p>
               <p className="WindBottom">Wind Speed</p>
             </div>
           </div>
